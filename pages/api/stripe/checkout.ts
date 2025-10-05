951b80eee2ff31b175102dd7b4f8c16b9eca8a93
enricosig <122876099+enricosig@users.noreply.github.com>
2025-10-05 08:38:29 +0200

    Update checkout.ts

--- pages/api/stripe/checkout.ts
+++ pages/api/stripe/checkout.ts
@@ -2,7 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 // Usa la versione API da ENV o fallback compatibile con il tuo SDK
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '');
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+  apiVersion: '2024-06-20',
+});
+
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
